5dcd2b81e6ced516964689185fa28ff3816e4dca
katatsuno <taka.tatsuno@gmail.com>
2025-04-17 17:31:50 +0900

    デプロイ修正2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,10 @@
-// pages/_app.tsx
 import { ClerkProvider } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      navigate={(to) => router.push(to)}
-    >
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
